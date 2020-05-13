af05b81520c584f71568826e133b19b65bd97f30
Ranjith-kumar03 <ranjithkumar03.1980@gmail.com>
2020-05-13 13:27:04 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder , Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './Shared/userName.Validator';
+import { PasswordValidator } from './Shared/password.validator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactive-Forms';
+  get UserName()
+  {
+    return this.registrationform.get('username');
+  }
+
+  constructor(private builder: FormBuilder)
+  {
+
+  }
+  registrationform =this.builder.group({
+    username : ["" , [Validators.required , Validators.minLength(3),forbiddenNameValidator(/suresh/)
+    ,forbiddenNameValidator(/username/),forbiddenNameValidator(/jukerberg/),forbiddenNameValidator(/mark/)]],
+    password : [],
+    confirmPassword : [],
+    address : this.builder.group({
+     city : ["chennai"],
+     state : ["tamil nadu"],
+     postal : [89989999]
+    })
+  },{validator: PasswordValidator})
+ /* registrationform =  new FormGroup({
+   username : new FormControl('Ranjith Kumar'),
+   password : new FormControl('airforce'),
+   confirmPassword : new FormControl('airforce'),
+   address : new FormGroup({
+    city : new FormControl('Ranjith Kumar'),
+    state : new FormControl('airforce'),
+    postal : new FormControl('airforce'),
+   }) */
+
+
+ loadData()
+ {
+   this.registrationform.patchValue({
+    username :"Ranjugggggggggggggggggggg",
+    password : "pullanhhhhhhhhhhh",
+    confirmPassword : "pullanuuuuuuuuuuuuuuuuuu",
+
+
+  });
+  }
+
 }
