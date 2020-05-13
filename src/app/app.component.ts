be5328831bf9e9ff73fd4ed1e651f14e866f9df2
Ranjith-kumar03 <ranjithkumar03.1980@gmail.com>
2020-05-13 23:59:54 +0530

    reactive finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder , Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder , Validators, FormGroup , FormArray} from '@angular/forms';
 import { forbiddenNameValidator } from './Shared/userName.Validator';
 import { PasswordValidator } from './Shared/password.validator';
 
@@ -8,27 +8,60 @@ import { PasswordValidator } from './Shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  registrationform: FormGroup;
   get UserName()
   {
     return this.registrationform.get('username');
   }
+  get Email()
+  {
+    return this.registrationform.get('email');
+  }
+
+  get alternateEmails()
+  {
+    return this.registrationform.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmails()
+  {
+   this.alternateEmails.push(this.builder.control(''));
+  }
+
 
   constructor(private builder: FormBuilder)
   {
 
   }
-  registrationform =this.builder.group({
-    username : ["" , [Validators.required , Validators.minLength(3),forbiddenNameValidator(/suresh/)
-    ,forbiddenNameValidator(/username/),forbiddenNameValidator(/jukerberg/),forbiddenNameValidator(/mark/)]],
-    password : [],
-    confirmPassword : [],
-    address : this.builder.group({
-     city : ["chennai"],
-     state : ["tamil nadu"],
-     postal : [89989999]
-    })
-  },{validator: PasswordValidator})
+  ngOnInit(): void {
+    this.registrationform =this.builder.group({
+      username : ["" , [Validators.required , Validators.minLength(3),forbiddenNameValidator(/suresh/)
+      ,forbiddenNameValidator(/username/),forbiddenNameValidator(/jukerberg/),forbiddenNameValidator(/mark/)]],
+      email: [],
+      subscribe: [false],
+      password : [],
+      confirmPassword : [],
+      address : this.builder.group({
+       city : ["chennai"],
+       state : ["tamil nadu"],
+       postal : [89989999]
+      }),alternateEmails: this.builder.array([])
+    },{validator: PasswordValidator});
+
+    this.registrationform.get("subscribe").valueChanges.subscribe(checkedValue =>{
+     const email =  this.registrationform.get("email");
+     if(checkedValue)
+     {
+       email.setValidators(Validators.required)
+     }else{
+        email.clearValidators();
+     }
+     email.updateValueAndValidity();
+    });
+
+  }
+
  /* registrationform =  new FormGroup({
    username : new FormControl('Ranjith Kumar'),
    password : new FormControl('airforce'),
